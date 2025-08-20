3104ab14c5cbebbb4c1427da16df2d41a4b381d4
onyislo <bradleyodwori@outlook.com>
2025-08-20 11:29:20 -0700

    add vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
+//import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    optimizeDeps: {
-      exclude: ['lucide-react'],
-    },
-    define: {
-      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.SUPABASE_URL || ''),
-      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.SUPABASE_ANON_KEY || ''),
-    }
-  };
-});
+//export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+//});
